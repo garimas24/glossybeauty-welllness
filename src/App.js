a0b40815171e2ea36b81f615384299e012af13c5
Garima Dixit <dixitgarima24@gmail.com>
2025-10-04 23:45:41 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 import Navbar from './components/Navbar/Navbar.jsx';
 // import Home from '../src/components/Navbar';
 import Academy from './components/Academy/Academy.jsx';
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,34 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-// import icon from '../../public/pictures/icon.jpeg'
 
-
-// FontAwesome CSS can also be placed in index.html or imported globally, but here's inline import if needed:
-// import 'font-awesome/css/font-awesome.min.css';
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const myFunction = () => {
+  const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
     <div className="heading">
-  <div className="navbar-container">
-    <div className="logo">
-      <img src="/pictures/icon.jpeg" alt="icon" />
+      <div className="navbar-container">
+        <div className="logo">
+          <img src="/pictures/icon.jpeg" alt="icon" />
+        </div>
+        <div className={`topnav ${menuOpen ? 'responsive' : ''}`} id="myTopnav">
+          <Link className="active" to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/services">Our Services</Link>
+          <Link to="/academy">Academy</Link>
+          <Link to="/packages">Package</Link>
+          <Link to="/more-services">Services</Link>
+          <Link to="/contact">Contact Us</Link>
+          <span className="icon" onClick={toggleMenu}>
+            <i className="fa fa-bars"></i>
+          </span>
+        </div>
+      </div>
     </div>
-    <div className={`topnav ${menuOpen ? 'responsive' : ''}`} id="myTopnav">
-      <a className="active" href="#home">Home</a>
-      <a href="#AboutUs">About Us</a>
-      <a href="#OurServices">Our Services</a>
-      <Link to="/academy">Academy</Link>
-      <a href="#Package">Package</a>
-      <a href="#Services">Services</a>
-      <a href="#ContactUs">Contact Us</a>
-      <a href="javascript:void(0);" className="icon" onClick={myFunction}>
-        <i className="fa fa-bars"></i>
-      </a>
-    </div>
-  </div>
-</div>
-
   );
 };
 
